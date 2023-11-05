0b5e4656fcb39a26118a76017d296eca7d549e42
BigTogu <silvia.kenaan@gmail.com>
2023-11-05 17:41:35 +0100

    fix: visualization of the messages

--- server/index.js
+++ server/index.js
@@ -1,30 +1,30 @@
 import express from "express";
 import http from "http";
 import { Server as SocketServer } from "socket.io";
-import MongoClient from "./mongo/connection.js";
-import { insertChatMessages } from "./mongo/queries.js";
+import { getChatMessages, insertChatMessages } from "./mongo/queries.js";
+import { type } from "os";
 
 const app = express();
 const httpServer = http.createServer(app);
 const options = { cors: { origin: "http://localhost:5173" } };
 const io = new SocketServer(httpServer, options);
 
-// await getChatMessages().then((data) => {
-//   messages = data;
-// });
+let messages = [];
+
+await getChatMessages().then((messages) => {
+  return messages;
+});
 
 // action when connection is based
 io.on("connection", (socket) => {
-  // socket.emit("messages", messages);
+  socket.emit("messages", messages);
   socket.on("new-message", (data) => {
-    console.log(data, "data");
-    //está saliendo bien la data, no entiendo que pasa
     insertChatMessages(data);
-
-    // //permite que los mensajes se vean en la misma pantalla
-    // socket.emit("messages", data);
-    // // permite que los mensajes se vean en todas las pantallas
-    // socket.broadcast.emit("messages", data);
+    messages.push(data);
+    // // //permite que los mensajes se vean en la misma pantalla
+    socket.emit("messages", messages);
+    // // // permite que los mensajes se vean en todas las pantallas
+    socket.broadcast.emit("messages", messages);
   });
 });
 
